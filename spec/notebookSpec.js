fc9c93da8d70bc6ffad897ff0e11217baa2be474
katiewhitlock <katiejwhitlock@gmail.com>
2021-03-16 15:01:11 +0000

    template tests added

--- spec/notebookSpec.js
+++ spec/notebookSpec.js
@@ -1,1 +1,38 @@
-console.log("Hello")
+"use strict";
+
+function expect(a){
+  return {
+    toEqual: function(b){
+      if (a == b) {
+      console.log('pass!') } else {
+      console.log('fail!')
+    }
+  },
+  toBeAnArray: function(){
+    if (a.constructor.name == "Array" ) {
+      console.log('pass!') } else {
+      console.log('fail!')
+    }
+  }
+  }
+
+}
+
+function it(label, callback) {
+console.log(`Test: ${label}`)
+callback()
+}
+
+var hello = []
+///------
+it('2 + 2 is equal to 4', function (){
+    expect(2+2).toEqual(4)
+  })
+
+it('2 + 3 is equal to 4', function (){
+  expect(2+3).toEqual(4)
+})
+
+it('outputs an array', function () {
+  expect(hello).toBeAnArray()
+})
