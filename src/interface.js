2d50866f32aadde856f27620e02ecf4e2bd09d96
Aniruddh <ojhaa94@gmail.com>
2021-03-17 16:18:24 +0000

    adds innerHTML to list element

--- src/interface.js
+++ src/interface.js
@@ -9,5 +9,11 @@
     notebook.create(note)
   } )
 
+	var noteList = document.getElementById("note-list");
+	noteList.innerHTML = '<li> This is a note! </li>'
+	// array is notebook.notes
+	// iterate through notebook.notes to print each note
+	// each note is printed inside <li> note </li>
+
   
 
